154e9a0e2b35c70b1878a39fe2c48d9bdb0aa1cb
Evilbas <dimonloboto@gmail.com>
2018-05-31 18:49:43 -0400

    Nothing interesting

--- src/main/java/com/evilbas/dyndbdemo/Config.java
+++ src/main/java/com/evilbas/dyndbdemo/Config.java
@@ -1,5 +1,8 @@
 package com.evilbas.dyndbdemo;
 
+import javax.sql.DataSource;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -7,6 +10,13 @@ import com.evilbas.dyndbcache.DynCache;
 
 @Configuration
 public class Config {
+	
+	@Bean
+	DataSource dataSource() {
+		DataSourceBuilder builder = new DataSourceBuilder();
+		builder.
+		return new DataSourceBuilder();
+	}
 	
 	@Bean
 	DynCache dynCache() {
